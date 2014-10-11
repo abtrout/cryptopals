debe4c128fd2395f69165a34681a28e5f2b9edb9
David Cochran <about.trout@gmail.com>
2014-10-11 15:14:10 -0500

    Add hamming distance of byte arrays

--- src/main/scala/utils/Hamming.scala
+++ src/main/scala/utils/Hamming.scala
@@ -1,9 +1,13 @@
 package net.logitank.cryptopals
 
 object Hamming {
+
   private def bitsum(b: Int) = (0 to 7).map(i => (b >> i) & 1).sum
 
-  def distance(s1: String, s2: String) = {
-    s1.getBytes.zip(s2.getBytes).map(b => bitsum(b._1 ^ b._2)).sum
+  def distance(as: Array[Byte], bs: Array[Byte]) = {
+    as.zip(bs).map(ab => bitsum(ab._1 ^ ab._2)).sum
   }
+  
+  // for convenience:
+  def distance(s1: String, s2: String): Int = distance(s1.getBytes, s2.getBytes)
 }
