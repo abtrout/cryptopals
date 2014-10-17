b83f7307083c0988efa7f1883c8c938f12453af3
David Cochran <about.trout@gmail.com>
2014-10-16 21:59:31 -0500

    finished challenge 6

--- src/test/scala/Set1.scala
+++ src/test/scala/Set1.scala
@@ -3,7 +3,7 @@ package net.logitank.cryptopals
 import org.specs2.mutable.Specification
 
 class Set1Tests extends Specification {
-  
+
   "challenge1" should {
     "convert given hex input to desired base64 output" in {
       val input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
@@ -24,7 +24,7 @@ class Set1Tests extends Specification {
       HexUtil.fromBytes(fixedXOR) mustEqual answer
     }
   }
-      
+
   def rankGuess(k: Char, bytes: Array[Byte]) = {
     val guess = bytes.map(b => (b ^ k).toChar).mkString
     // note that "uldrhs nioate" = "etaoin shrdlu".reverse
@@ -55,7 +55,7 @@ class Set1Tests extends Specification {
       val lines = Source.fromURL(getClass.getResource("/challenge-data/4.txt")).getLines
       val (key, line, _) = lines.flatMap(line => {
         val bytes = HexUtil.toBytes(line)
-        (32 to 126).map(k => (k.toChar, line, rankGuess(k.toChar, bytes))) 
+        (32 to 126).map(k => (k.toChar, line, rankGuess(k.toChar, bytes)))
       }).maxBy(_._3)
 
       key mustEqual '5'
@@ -82,28 +82,128 @@ class Set1Tests extends Specification {
 
   "challenge6" should {
     import scala.io.Source
-    val input = Source.fromURL(getClass.getResource("/challenge-data/6.txt")).getLines.mkString
-
-    "compute Hamming distance of two strings" in {
-      Hamming.distance("this is a test", "wokka wokka!!!") mustEqual 37
+    val input = Base64Util.decode {
+      Source.fromURL(getClass.getResource("/challenge-data/6.txt")).getLines.mkString
     }
 
-    /*
-    "find key size" in {
+    "find key length using Hamming distance" in {
+      val numBlocks = 5
       val keysize = (2 to 40).map(k => {
-        // rank this keysize by using Hamming distance on keysize-length sub blocks of input
-        //Hamming.distance(input.substring(0, k), input.substring(k, 2*k))/k
+        val blocks = input.grouped(k).take(numBlocks).toList
+        val avg = (0 until numBlocks-1).flatMap(i => {
+          (i+1 until numBlocks).map(j => Hamming.distance(blocks(i), blocks(j)))
+        }).sum / (2*k)
 
-        // finer tuning:
-        val numBlocks = 2 
-        input.grouped(k).grouped(2).take(numBlocks).map(ab => {
-          val List(a,b) = ab
-          dist = Hamming.distance(a, b)
-        }).sum / numBlocks
-      }).min
+        (k, avg)
+      }).minBy(_._2)._1
 
-      keysize mustEqual 2
+      keysize mustEqual 29
     }
-    */
+
+    "compute key from repeating-key XOR" in {
+      val keysize = 29
+      val blockLength = input.length / keysize
+
+      val key = (0 until keysize).map(i => {
+        val block = (0 until blockLength).map(j => {
+          input.charAt(i + (j*keysize)).toByte
+        }).toArray
+
+        (32 to 126).map(k => (k.toChar, rankGuess(k.toChar, block))).maxBy(_._2)._1
+      }).mkString
+
+      key mustEqual "Terminator X: Bring the noise"
+    }
+
+    "decode plaintext" in {
+
+      val key = "Terminator X: Bring the noise"
+      val plaintext = input.zipWithIndex.map(x => {
+        (x._1.toByte ^ key.charAt(x._2 % key.length)).toChar
+      }).mkString
+
+      plaintext mustEqual Seq(
+        "I'm back and I'm ringin' the bell ",
+        "A rockin' on the mike while the fly girls yell ",
+        "In ecstasy in the back of me ",
+        "Well that's my DJ Deshay cuttin' all them Z's ",
+        "Hittin' hard and the girlies goin' crazy ",
+        "Vanilla's on the mike, man I'm not lazy. ",
+        "",
+        "I'm lettin' my drug kick in ",
+        "It controls my mouth and I begin ",
+        "To just let it flow, let my concepts go ",
+        "My posse's to the side yellin', Go Vanilla Go! ",
+        "",
+        "Smooth 'cause that's the way I will be ",
+        "And if you don't give a damn, then ",
+        "Why you starin' at me ",
+        "So get off 'cause I control the stage ",
+        "There's no dissin' allowed ",
+        "I'm in my own phase ",
+        "The girlies sa y they love me and that is ok ",
+        "And I can dance better than any kid n' play ",
+        "",
+        "Stage 2 -- Yea the one ya' wanna listen to ",
+        "It's off my head so let the beat play through ",
+        "So I can funk it up and make it sound good ",
+        "1-2-3 Yo -- Knock on some wood ",
+        "For good luck, I like my rhymes atrocious ",
+        "Supercalafragilisticexpialidocious ",
+        "I'm an effect and that you can bet ",
+        "I can take a fly girl and make her wet. ",
+        "",
+        "I'm like Samson -- Samson to Delilah ",
+        "There's no denyin', You can try to hang ",
+        "But you'll keep tryin' to get my style ",
+        "Over and over, practice makes perfect ",
+        "But not if you're a loafer. ",
+        "",
+        "You'll get nowhere, no place, no time, no girls ",
+        "Soon -- Oh my God, homebody, you probably eat ",
+        "Spaghetti with a spoon! Come on and say it! ",
+        "",
+        "VIP. Vanilla Ice yep, yep, I'm comin' hard like a rhino ",
+        "Intoxicating so you stagger like a wino ",
+        "So punks stop trying and girl stop cryin' ",
+        "Vanilla Ice is sellin' and you people are buyin' ",
+        "'Cause why the freaks are jockin' like Crazy Glue ",
+        "Movin' and groovin' trying to sing along ",
+        "All through the ghetto groovin' this here song ",
+        "Now you're amazed by the VIP posse. ",
+        "",
+        "Steppin' so hard like a German Nazi ",
+        "Startled by the bases hittin' ground ",
+        "There's no trippin' on mine, I'm just gettin' down ",
+        "Sparkamatic, I'm hangin' tight like a fanatic ",
+        "You trapped me once and I thought that ",
+        "You might have it ",
+        "So step down and lend me your ear ",
+        "'89 in my time! You, '90 is my year. ",
+        "",
+        "You're weakenin' fast, YO! and I can tell it ",
+        "Your body's gettin' hot, so, so I can smell it ",
+        "So don't be mad and don't be sad ",
+        "'Cause the lyrics belong to ICE, You can call me Dad ",
+        "You're pitchin' a fit, so step back and endure ",
+        "Let the witch doctor, Ice, do the dance to cure ",
+        "So come up close and don't be square ",
+        "You wanna battle me -- Anytime, anywhere ",
+        "",
+        "You thought that I was weak, Boy, you're dead wrong ",
+        "So come on, everybody and sing this song ",
+        "",
+        "Say -- Play that funky music Say, go white boy, go white boy go ",
+        "play that funky music Go white boy, go white boy, go ",
+        "Lay down and boogie and play that funky music till you die. ",
+        "",
+        "Play that funky music Come on, Come on, let me hear ",
+        "Play that funky music white boy you say it, say it ",
+        "Play that funky music A little louder now ",
+        "Play that funky music, white boy Come on, Come on, Come on ",
+        "Play that funky music ",
+        ""
+      ).mkString("\n")
+    }
   }
 }
