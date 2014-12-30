8c1c2711c0ac2b56e6eac602f2bfa7c804f455b1
David Cochran <about.trout@gmail.com>
2014-12-30 13:48:02 -0600

    add PKCS#7 padding to CryptoUtil. add encrypt/decrypt convenience methods to AES

--- src/main/scala/utils/CryptoUtil.scala
+++ src/main/scala/utils/CryptoUtil.scala
@@ -1,9 +1,22 @@
-// NB: This implements AES-128 ECB, which is bad crypto.
-// Don't use it!
-// This is for a cryptopals (http://cryptopals.com) challenge.
 package net.logitank.cryptopals.CryptoUtil
 
+
+object Padding {
+  implicit class StringPadding(val s: String) {
+    // PKCS#7 <http://tools.ietf.org/html/rfc5652#section-6.3>
+    def pkcs7(n: Int): String =
+      (n - s.length) match {
+        case k: Int if k > 0 => s.padTo(n, k.toChar)
+        case k: Int if k == 0 => s
+        case _ => throw new IllegalArgumentException
+      }
+  }
+}
+
 object AES {
+
+  // NB: This implements AES-128 ECB, which is bad crypto. Don't use it!
+  // This is for a cryptopals (http://cryptopals.com) challenge.
 
   import javax.crypto.Cipher
   import javax.crypto.spec.SecretKeySpec
@@ -16,27 +29,19 @@ object AES {
     cipher.doFinal(pbytes)
   }
 
-  def encrypt(plaintext: String, key: String): String = {
-    val pbytes = plaintext.map(_.toByte).toArray
-    val kbytes = key.getBytes
-
-    encrypt(pbytes, kbytes).map(_.toChar).mkString
-  }
-  
   def decrypt(cbytes: Array[Byte], kbytes: Array[Byte]) = {
     val key = new SecretKeySpec(kbytes, "AES")
     val cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
     cipher.init(Cipher.DECRYPT_MODE, key)
 
-    println(kbytes.length, cbytes.length)
-
     cipher.doFinal(cbytes)
   }
 
-  def decrypt(ciphertext: String, key: String): String = {
-    val cbytes = ciphertext.map(_.toByte).toArray
-    val kbytes = key.getBytes
+  def encrypt(plaintext: String, key: String): String =
+    // note: .map(_.toByte) vs .getBytes important here in case
+    // our ciphertext/plaintext contains UTF8 characters, it seems
+    encrypt(plaintext.map(_.toByte).toArray, key.getBytes).map(_.toChar).mkString
 
-    decrypt(cbytes, kbytes).map(_.toChar).mkString
-  }
+  def decrypt(ciphertext: String, key: String): String =
+    decrypt(ciphertext.map(_.toByte).toArray, key.getBytes).map(_.toChar).mkString
 }
