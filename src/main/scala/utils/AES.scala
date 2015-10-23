8b937a97d228faca1693fcd30aefc4d594c7c42d
david cochran <about.trout@gmail.com>
2015-10-22 21:40:29 -0500

    rework AESUtil to deal with bytes, rename to AES

--- src/main/scala/utils/AES.scala
+++ src/main/scala/utils/AES.scala
@@ -3,47 +3,66 @@ package net.logitank.cryptopals.AES
 import javax.crypto.Cipher
 import javax.crypto.spec.SecretKeySpec
 
+import net.logitank.cryptopals.Padding._
+
 object CBC {
 
-  private def fixedXOR(a: String, b: String) =
-    a.zip(b).map(ab => (ab._1 ^ ab._2).toByte)
-  
-  def decrypt(ctext: String, key: String, iv: String) = {
-    (iv + ctext)
-      .grouped(key.length).sliding(2)
-      .flatMap(x => {
-        val Seq(previousBlock, currentBlock) = x
-        fixedXOR(ECB.decrypt(currentBlock, key), previousBlock)
-      })
-      .map(_.toChar).mkString
+  private def fixedXOR(as: Array[Byte], bs: Array[Byte]): Array[Byte] =
+    as.zip(bs).map(ab => (ab._1 ^ ab._2).toByte)
+
+  def encrypt(pbytes: Array[Byte], kbytes: Array[Byte], iv: Array[Byte]) = {
+    val blocks =
+      pbytes.pkcs7(kbytes.length)
+        .grouped(kbytes.length)
+        .foldLeft(List(iv)) { (cs, p) =>
+          val input = fixedXOR(cs.head, p)
+          ECB.encrypt(input, kbytes) :: cs
+        }
+
+    blocks.reverse.tail.flatten.toArray
   }
 
-  // def encrypt(ctext: String, key: String, iv: String) = {}
+  def decrypt(cbytes: Array[Byte], kbytes: Array[Byte], iv: Array[Byte]) = {
+    (iv ++ cbytes)
+      .grouped(kbytes.length).sliding(2)
+      .flatMap { x =>
+        val Seq(previousBlock, currentBlock) = x
+        fixedXOR(ECB.decrypt(currentBlock, kbytes), previousBlock)
+      }
+  }
 }
 
-// NB: This implements AES-128 ECB, which is bad crypto. Don't use it!
-// This is for a cryptopals (http://cryptopals.com) challenge.
 object ECB {
+  // NB: This implements AES-128 ECB, which is bad crypto. Don't use it!
+  // This is for a cryptopals (http://cryptopals.com) challenge.
 
-  def encrypt(plaintext: String, key: String): String =
-    encrypt(plaintext.map(_.toByte).toArray, key.getBytes).map(_.toChar).mkString
+  def encrypt(plaintext: String, key: String): String = {
+    val pbytes = plaintext.toCharArray.map(_.toByte)
+    val kbytes = key.toCharArray.map(_.toByte)
 
-  def decrypt(ciphertext: String, key: String): String =
-    decrypt(ciphertext.map(_.toByte).toArray, key.getBytes).map(_.toChar).mkString
+    encrypt(pbytes, kbytes).map(_.toChar).mkString
+  }
 
   def encrypt(pbytes: Array[Byte], kbytes: Array[Byte]) = {
     val key = new SecretKeySpec(kbytes, "AES")
     val cipher = Cipher.getInstance("AES/ECB/NoPadding")
-    cipher.init(Cipher.ENCRYPT_MODE, key)
 
+    cipher.init(Cipher.ENCRYPT_MODE, key)
     cipher.doFinal(pbytes)
+  }
+
+  def decrypt(ciphertext: String, key: String): String = {
+    val cbytes = ciphertext.toCharArray.map(_.toByte)
+    val kbytes = key.toCharArray.map(_.toByte)
+
+    decrypt(cbytes, kbytes).map(_.toChar).mkString
   }
 
   def decrypt(cbytes: Array[Byte], kbytes: Array[Byte]) = {
     val key = new SecretKeySpec(kbytes, "AES")
     val cipher = Cipher.getInstance("AES/ECB/NoPadding")
-    cipher.init(Cipher.DECRYPT_MODE, key)
 
+    cipher.init(Cipher.DECRYPT_MODE, key)
     cipher.doFinal(cbytes)
   }
 }
